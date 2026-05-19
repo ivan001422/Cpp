3946304b6b12c6d609196e7faf4a6054be12b68a
Новиков Иван <148786186+ivan001422@users.noreply.github.com>
2026-05-19 14:40:01 +0300

    Create input&output.cpp

---
+++ Cpp_list/Syntax@program_structure/input&output.cpp
@@ -0,0 +1,14 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+	int a, b;
+	string x;
+
+	cin >> a >> b >> x; // Важно чтобы при вводе данных стоял пробел
+
+
+}
